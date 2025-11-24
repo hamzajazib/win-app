730618eb245379224eec80b6bde6adac110c9331
Mindaugas Veblauskas <mindaugas.veblauskas@proton.ch>
2025-11-24 12:07:34 +0200

    Fix app uninstall in UI tests

--- ci/test-scripts/uninstall-app.ps1
+++ ci/test-scripts/uninstall-app.ps1
@@ -1,6 +1,17 @@
 $protonFolder = "C:\Program Files\Proton\VPN"
 $protonUninstallExe = $protonFolder + "\unins000.exe"
+
 if (Test-Path -Path $protonFolder) {
     Start-Process -FilePath $protonUninstallExe -ArgumentList "/verysilent" -Wait -ErrorAction Ignore
+
+    # Sometimes the callout driver is stuck running
+    net stop ProtonVPNCallout 2>$null
+
     Remove-Item $protonFolder -Recurse -ErrorAction Ignore
+}
+
+# If the uninstaller failed for any reason, clean the registry manually
+$uninstallKey = "HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\Proton VPN_is1"
+if (Test-Path $uninstallKey) {
+    Remove-Item $uninstallKey -Recurse -Force -ErrorAction Ignore
 }
